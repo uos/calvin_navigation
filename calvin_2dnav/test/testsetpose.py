6086aaa5374d155fc643a192c3ff9560f1a1305c
Michael Stypa <mstypa@uos.de>
2013-09-23 16:48:13 +0200

    working set pose estimate for amcl test script

--- calvin_2dnav/test/testsetpose.py
+++ calvin_2dnav/test/testsetpose.py
@@ -2,6 +2,7 @@
 import roslib; roslib.load_manifest('calvin_2dnav')
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
+import random
 
 if __name__ == '__main__':
   try:
@@ -10,12 +11,15 @@ if __name__ == '__main__':
     pose = PoseWithCovarianceStamped()
     pose.header.frame_id = "/map"
     pose.header.stamp = rospy.Time.now()
-    pose.pose.pose.position.x = 5
-    pose.pose.pose.position.y = 5
-    pose.pose.pose.position.z = 0
-    pose.pose.covariance[0] = 0.7
-    pose.pose.covariance[35] = 0.7
-    rospy.loginfo("Pose sent: %d %d %d", pose.pose.pose.position.x, pose.pose.pose.position.y, pose.pose.pose.position.z)
+    pose.pose.pose.position.x = random.randint(1,5)
+    pose.pose.pose.position.y = random.randint(1,5)
+    pose.pose.pose.position.z = 1.0
+    pose.pose.pose.orientation.x = 0.0
+    pose.pose.pose.orientation.y = 0.0
+    pose.pose.pose.orientation.z = 0.0
+    pose.pose.pose.orientation.w = 1.0
+    rospy.sleep(0.1)
     pub.publish(pose)
+    rospy.loginfo("Pose sent: %d %d %d", pose.pose.pose.position.x, pose.pose.pose.position.y, pose.pose.pose.position.z)
   except rospy.ROSInterruptException:
     rospy.logerror("exception")
